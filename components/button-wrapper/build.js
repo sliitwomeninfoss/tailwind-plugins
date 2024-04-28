b3951e093db5780051ca714dbe2cae7d3ebed5a5
SandalikaAriyrathna <dilsandu96@gmail.com>
2024-04-28 20:52:00 +0530

    FIX: esbuild

--- components/button-wrapper/build.js
+++ components/button-wrapper/build.js
@@ -3,12 +3,12 @@ const esbuild = require("esbuild");
 try {
   esbuild
     .build({
-      entryPoints: ["src/index.js"],
+      entryPoints: ["src/Button.js"],
       bundle: true,
       minify: true,
-      platform: "node",
-      target: "node14",
-      outfile: "dist/index.js",
+      platform: "browser",
+      target: "es2020",
+      outfile: "dist/Button.js",
       loader: { ".js": "jsx" },
       jsxFactory: "React.createElement",
       jsxFragment: "React.Fragment",
